7b10233ce6ccdd7fb9aaeb6ac4d439457400a877
mehrozh <150233494+mehrozh-fusionwave@users.noreply.github.com>
2024-03-07 00:02:25 +0500

    theme

--- lib/view/app_theme/theme_bloc/theme_bloc.dart
+++ lib/view/app_theme/theme_bloc/theme_bloc.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:tericce_animation/view/app_theme/theme_helper/theme_helper.dart';
 part 'theme_event.dart';
-part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
   ThemeBloc() : super(ThemeData.dark()) {
